3f9f5cb233718fbd3108bab647c895c153d24e73
Rowan Manning <info@rowanmanning.com>
2014-06-09 15:28:21 +0100

    Quick first build of the pa11y module

--- test/pa11y.js
+++ test/pa11y.js
@@ -13,4 +13,56 @@
 // You should have received a copy of the GNU General Public License
 // along with pa11y.  If not, see <http://www.gnu.org/licenses/>.
 
+/* jshint maxstatements: false, maxlen: false */
+/* global afterEach, beforeEach, describe, it */
 'use strict';
+
+var assert = require('proclaim');
+var mockery = require('mockery');
+var sinon = require('sinon');
+
+describe('pa11y', function () {
+	var pa11y, truffler;
+
+	beforeEach(function () {
+		mockery.enable({
+			useCleanCache: true,
+			warnOnUnregistered: false,
+			warnOnReplace: false
+		});
+		truffler = {
+			init: sinon.stub()
+		};
+		mockery.registerMock('truffler', truffler);
+		pa11y = require('../lib/pa11y');
+	});
+
+	afterEach(function () {
+		mockery.deregisterAll();
+		mockery.disable();
+	});
+
+	it('should be an object', function () {
+		assert.isObject(pa11y);
+	});
+
+	it('should have an `init` method', function () {
+		assert.isFunction(pa11y.init);
+	});
+
+	describe('.init()', function () {
+
+		it('should create a truffler instance with the correct config', function () {
+			pa11y.init({});
+			assert.strictEqual(truffler.init.callCount, 1);
+		});
+
+		it('should return the truffler test function', function () {
+			var test = function () {};
+			truffler.init.returns(test);
+			assert.strictEqual(pa11y.init(), test);
+		});
+
+	});
+
+});
